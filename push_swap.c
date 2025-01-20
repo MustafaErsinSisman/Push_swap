f259dc87d0555ed527f7743a0a2acacc8b4c9c17
Mustafa Ersin Şişman <mustafaersinsisman@gmail.com>
2025-01-21 01:52:52 +0300

    integer  olup olmama  hata koşulu eklendi ama  max ve  min  değerlerde kontrol hatalı atoi kullanılmasına rağmen

--- push_swap.c
+++ push_swap.c
@@ -16,9 +16,11 @@ int not_Integer(char *s)
 {
         int i;
 
-        i  = 0;
-        while (s[i])
-                if (s[i] >=  '0' && s[i] <= '9')
+        if(!s)
+                return (0);
+        i  = -1;
+        while (s[++i])
+                if (s[i] >= '0' && s[i] <= '9')
                         return  (1);
         return  (0);
 }
@@ -28,24 +30,27 @@ int main(int ac, char **av)
         if(ac >= 2)
         {
                 int i;
-                int j;
                 int dc;
 
-                dc = ft_strlen(av);
+                dc = ac - 1;
                 i = 0;
-                j = 0;
                 while (dc > 0)
                 {
                         if(!not_Integer(av[dc]))
                         {
                                 ft_printf("Error\n");
-                                break;
+                                return (0);
                         }
-                        while (++i < ac)
-                                ft_printf("%s ", &av[i][0]);
                         dc--;
                 }
-                
+                while (++i < ac)
+                        if((int)ft_atoi(av[i]) > 2147143647 || (int)ft_atoi(av[i]) < -2147143648)
+                        {        
+                                ft_printf("Error\n");
+                                return (0);
+                        }
+                        else
+                                ft_printf("%s ", av[i]);
                 ft_printf("\n");
         }
         else
